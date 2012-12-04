5134c3cf3371b43b5dd3a080d8f8a6e84f64a34a
Jakub Buczyński <jbuczynski@p109-14.labpk.inf.ug.edu.pl>
2012-12-04 09:50:34 +0100

    lab6

---
+++ lab6.sh
@@ -0,0 +1,20 @@
+zad1.
+grep "[[:alnum:]{1,}]" text1.txt
+zad2.
+grep ^"[0-9]" pl*
+zad3.
+
+egrep -l "^.{8}r" *
+zad4.
+
+grep -c bash /etc/passwd// wyswietla lancuch wybników jako liczbe
+grep  bash /etc/passwd
+zad5.
+grep [IVXLMCD] plik2.txt
+
+
+
+
+
+
+
